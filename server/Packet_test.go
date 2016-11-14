983efbe281afe537abaa586833df9a45f86a0288
Spriithy <theo.dano@gmail.com>
2016-11-14 06:28:53 +0100

    test

--- server/Packet_test.go
+++ server/Packet_test.go
@@ -4,14 +4,14 @@ import (
 	"testing"
 )
 
-func TestTimeStampParsing(t *testing.T) {
-	source := map[string]bool{
-		"15:42:13": true, "00:60:00": false, "01:17:31": true,
-		"17;32:00": false, "abcdefgh": false, "-1:-0:18": false,
-		"34:15:22": false, "  :  :  ": false, "06:18:20": true,
-	}
+var sourceStamps = map[string]bool{
+	"15:42:13": true, "00:60:00": false, "01:17:31": true,
+	"17;32:00": false, "abcdefgh": false, "-1:-0:18": false,
+	"34:15:22": false, "  :  :  ": false, "06:18:20": true,
+}
 
-	for s, v := range source {
+func TestTimeStampParsing(t *testing.T) {
+	for s, v := range sourceStamps {
 		if ts, err := ParseTimeStamp(s); err != nil {
 			if v {
 				t.Errorf("%s should have matched TimeStamp", s)
@@ -22,8 +22,4 @@ func TestTimeStampParsing(t *testing.T) {
 			t.Log(s, "matched", ts.String())
 		}
 	}
-}
-
-func TestPacketsParsing(t *testing.T) {
-	// TODO
 }
